1ab81a928ca7f0241be226e46dbf04d8f5235a9a
ducksfrogs <ma_yamaki@yahoo.com>
2024-09-06 17:23:23 +0900

    school

--- sec/src/js/main.js
+++ sec/src/js/main.js
@@ -1,4 +1,11 @@
+import "../style/styles.scss";
 import * as d3 from 'd3';
 
 
-d3.selectAll("p").style('color', 'red');
+// d3.selectAll("p").style('color', 'red');
+
+// const a = document.querySelector(".canva")
+
+const b = d3.select(".canva")
+
+console.log(b);``
