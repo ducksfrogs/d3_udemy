197b267798f26dd082e7d78775fd9788f079d782
ducksfrogs <ma_yamaki@yahoo.com>
2024-09-07 16:01:06 +0900

    line

--- lessons/sec/src/js/third.js
+++ lessons/sec/src/js/third.js
@@ -2,10 +2,19 @@ import * as d3 from 'd3';
 
 const canvas = d3.select(".canva");
 
-var dataArray = [4, 15, 34, 124, 23];
+// var dataArray = [4, 15, 34, 124, 23];
+var dataArray = [
+    {width: 25, height: 15, fill: 'pinl'},
+    {width: 25, height: 25, fill: 'pinl'},
+    {width: 25, height: 115, fill: 'pinl'},
+    {width: 25, height: 5, fill: 'pinl'},
+    {width: 25, height: 35, fill: 'pinl'},
+];
 
-const svg = canvas.select("svg")
-svg.attr("height", 300)
+const svg = canvas.append("svg")
+            // .attr('width', "50%")
+            .attr("width", 500)
+            .attr('height', 550);
 
 const rect = svg.selectAll("rect");
 
@@ -13,13 +22,13 @@ rect.data(dataArray)
     .enter().append("rect")
     .attr("width", 25)
     .attr("height", function(d) {
-    return d*2;
+    return d.height*2;
     })
     .attr("fill", "green")
     .attr("x", function(d , i) { 
         return i*27 } )
     .attr("y", function(d, i) {
-        return 300 - (d*2);
+        return 300 - (d.height*2);
     })                
 
 console.log(rect);
