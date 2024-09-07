14c6ce2a56dece5226666662b73333143a9b3f0f
ducksfrogs <ma_yamaki@yahoo.com>
2024-09-07 15:31:36 +0900

    d

--- lessons/sec/src/js/third.js
+++ lessons/sec/src/js/third.js
@@ -2,9 +2,10 @@ import * as d3 from 'd3';
 
 const canvas = d3.select(".canva");
 
-var dataArray = [4, 15, 34, 124, 230];
+var dataArray = [4, 15, 34, 124, 23];
 
 const svg = canvas.select("svg")
+svg.attr("height", 300)
 
 const rect = svg.selectAll("rect");
 
@@ -12,16 +13,13 @@ rect.data(dataArray)
     .enter().append("rect")
     .attr("width", 25)
     .attr("height", function(d) {
-    return d*2 ;
-
+    return d*2;
     })
     .attr("fill", "green")
     .attr("x", function(d , i) { 
-        console.log(i);
-        console.log(d);
         return i*27 } )
     .attr("y", function(d, i) {
-        return 100 - (d*2);
+        return 300 - (d*2);
     })                
 
 console.log(rect);
