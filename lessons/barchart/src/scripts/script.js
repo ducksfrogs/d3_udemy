4c1d6e63c089410237cfc813448bd72633d8e285
ducksfrogs <ma_yamaki@yahoo.com>
2024-09-10 18:17:46 +0900

    tooltip

--- lessons/barchart/src/scripts/script.js
+++ lessons/barchart/src/scripts/script.js
@@ -11,10 +11,19 @@ const rect = svg.selectAll("rect");
 
 d3.json("test.json")
         .then(data => {
+                console.log(data);
+                const y = d3.scaleLinear()
+                                .domain([0, d3.max(data, d => d.height)])
+                                .range([0, 250]);
+
+                console.log(y(0));
+                console.log(y(250));
+                console.log(y(500));
+
             rect.data(data)
                     .enter().append("rect")
                     .attr("width", (d, i) => d.width)
-                    .attr("height", (d, i) => d.height)
+                    .attr("height", (d, i) => y(d.height))
                     .attr("fill", (d) => d.fill)
                     .attr("x", (d, i) => i * 26)
                     
