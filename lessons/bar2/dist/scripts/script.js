88c0a600510e6d38ee24f39468c478af31c16083
ducksfrogs <ma_yamaki@yahoo.com>
2024-09-15 19:15:57 +0900

    begin svg line

--- lessons/bar2/dist/scripts/script.js
+++ lessons/bar2/dist/scripts/script.js
@@ -1,21 +1,18 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 const canvas = d3.select(".canva");
-var width = 600;
-var height = 600;
-const svg = canvas.append('svg').attr("width", width).attr("height", height);
-var div = d3.select("body").append("div");
-d3.json(api_url).then(data => {
-  const circle = svg.selectAll("circle").data(data.features);
-  circle.attr("cx", (d, i) => d.properties.mag).attr("cy", (d, i) => Math.floor(Math.random() * 100 + d.properties.mag)).attr("r", (d, i) => d.properties.mag).attr("fill", (d, i) => d.properties.alert);
-  circle.enter().append("circle").attr("cx", (d, i) => d.properties.mag * 23).attr("cy", (d, i) => Math.floor(Math.random() * 100 + d.properties.mag)).attr("r", (d, i) => d.properties.mag).attr("r", function (d, i, node) {
-    return d.properties.mag;
-  }).style('top', 125).on("mouseover", handleMouseOver).attr("fill", (d, i) => d.properties.alert);
-  function handleMouseOver(event) {
-    var mag = event.currentTarget.__data__.properties.mag;
-    console.log(event);
-    d3.select(event.currentTarget).style("opacity", 0.3);
-    console.log(mag);
-    div.transition().duration(200).style("opacity", 0.9);
-    div.html("<p> " + mag + "</p>").style("left", event.pageX + "px").style("top", event.pageY + "px");
-  }
+const margin = {
+  top: 10,
+  right: 10,
+  bottom: 10,
+  left: 10
+};
+``;
+const width = 640 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
+const svg = canvas.append("svg").attr("width", width).attr("height", height).attr("viewBox", [0, 0, width, height]).attr("style", "max-width: 100%; height: auto");
+d3.csv("./data/abc.csv").then(data => {
+  console.log(data);
+  const x = d3.scaleBand().domain(d3.groupSort(data, ([d]) => -d.frequency, d => d.letter)).range([margin.left, width - margin.right]).padding(0.1);
+  const y = d3.scaleLinear().domain([0, d3.max(data, d => d.frequency())]).range([height - margin.bottom, margin.top]);
+  svg.append("g").attr("fill", "seelblue").selectAll().data(data).join("rect").attr("x", d => x(d.letter)).attr("y", d => y(d.frequency)).attr("height", d => y(0) - y(d.frequency)).attr("width", x.bandwidth());
 });
--- lessons/observal/hello-framework/tmp.js
+++ lessons/observal/hello-framework/tmp.js
@@ -1,47 +1,8 @@
-# my first obeservable
-
-<div class="canva"></div>
-
-```js
 const   margin = { top: 10, right: 10, bottom: 10, left: 10 };``
 const   width = 640 - margin.left - margin.right;
 const   height = 400 - margin.top - margin.bottom;
 
-const canvas = d3.select('.canva');
-
-const x = d3.scaleUtc()
-        .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-        .range([margin.left, width - margin.right]);
-
-const y = d3.scaleLinear()
-        .domain([0, 100])
-        .range([height - margin.bottom, margin.top]);
-
-const svg = canvas.append("svg")
-                    .attr("width", 640)
-                    .attr("height", 400);
-
-
-svg.append("g")
-    .attr('transform', `translate(0, ${height - margin.bottom})` )
-    .call(d3.axisBottom(x));
-
-svg.append("g")
-    .attr('transform', `translate(${margin.left}, 0)`)
-    .call(d3.axisLeft(y));
-
-```
-
-## second 
-
-<div class="canva2"></div>
-
-```js
-const   margin = { top: 10, right: 10, bottom: 10, left: 10 };``
-const   width = 640 - margin.left - margin.right;
-const   height = 400 - margin.top - margin.bottom;
-
-const data = FileAttachment("/data/abc.csv").csv({typed: "auto"});
+// const data = FileAttachment("/data/abc.csv").csv({typed: "auto"});
 
 
 
@@ -53,9 +14,8 @@ const svg = canvas.append("svg")
                 .attr("viewBox", [0, 0, width, height])
                 .attr("style", "max-width: 100%; height: auto");
 
-d3.csv("./abc.csv")
+d3.csv('/data/abc.csv')
     .then(data => {
-        console.log(data);
         const x = d3.scaleBand()
                     .domain(d3.groupSorr(data, ([d]) => -d.frequency, (d) => d.letter))
                     .range([margin.left, width - margin.right])
@@ -76,5 +36,3 @@ d3.csv("./abc.csv")
 
 
     })
-
-```
